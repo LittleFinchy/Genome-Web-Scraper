699d799a762e93732f73bc4948ecf500d8ba2d18
LittleFinchy <57785785+LittleFinchy@users.noreply.github.com>
2020-07-03 22:41:16 -0400

    Update Genome.py

--- Genome.py
+++ Genome.py
@@ -6,8 +6,32 @@ wiki_data = urlopen(wiki_url)
 wiki_html = wiki_data.read()
 wiki_data.close()
 
-page_soup = soup(wiki_data, 'html.parser')
+page_soup = soup(wiki_html, 'html.parser')
 
 genome_table = page_soup.findAll('table', {'class': 'wikitable'})
+genome_table = genome_table[0]
 
-print(genome_table)
+headers = genome_table.findAll('th', {})
+
+header_titles = []
+for header in headers:
+    header_titles.append(header.text[:-1])
+
+rows = genome_table.findAll('tr', {})
+
+data = rows[1:]
+first_row = data[0]
+first_row_data = first_row.findAll('td', {})
+
+data_texts = []
+for data_text in first_row_data:
+    data_texts.append(data_text.text[:-1])
+
+table_rows = []
+for row in data:
+    table_row = []
+    row_data = row.findAll('td', {})
+    for data_point in row_data:
+        table_row.append(data_point.text[:-1])
+    table_rows.append(table_row)
+print(table_rows)
